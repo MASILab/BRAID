18825d2df4fc222c7e0b0e318331009c40c01b51
chenyugoal <chenyu_gao@outlook.com>
2023-11-09 21:28:46 -0600

    verify according to the new csv for ADNI

--- data/subject_info/scripts/ADNI_collect_info.py
+++ data/subject_info/scripts/ADNI_collect_info.py
@@ -84,9 +84,9 @@ for subject in path_dataset_bids.iterdir():
         try:
             values = df_mri_t1.loc[(df_mri_t1['subject_id']==subject_id)&(df_mri_t1['visit_bids']==visit_id), 'Research Group'].values
             values = values[~pd.isna(values)]
-            diagnosis = values[0]
+            diagnosis_group = values[0]
         except:
-            diagnosis = None
+            diagnosis_group = None
         
         try:
             values = df_mri_t1.loc[(df_mri_t1['subject_id']==subject_id)&(df_mri_t1['visit_bids']==visit_id), 'study_date'].values
@@ -125,15 +125,15 @@ for subject in path_dataset_bids.iterdir():
                 except:
                     sex = None
             
-            if diagnosis == None:
-                try:
-                    values = df_cognitive.loc[(df_cognitive['RID']==int(subject_id)) & (time_difference < pd.Timedelta(days=90)), 'PHC_Diagnosis'].values
-                    values = values[~pd.isna(values)]
-                    diagnosis = values[0]
-                    diagnosis = dict_diagnosis[diagnosis]
-                    print("Use PHC_Diagnosis as diagnosis")
-                except:
-                    diagnosis = None
+            # Prefer cognitive info from ADSP_PHC_COGN_10_05_22_30Aug2023.csv
+            try:
+                values = df_cognitive.loc[(df_cognitive['RID']==int(subject_id)) & (time_difference < pd.Timedelta(days=90)), 'PHC_Diagnosis'].values
+                values = values[~pd.isna(values)]
+                diagnosis = values[0]
+                diagnosis = dict_diagnosis[diagnosis]
+                # print("Use PHC_Diagnosis as diagnosis")
+            except:
+                diagnosis = diagnosis_group
         
         # Record
         list_df_subject.append(subject.name)
@@ -151,4 +151,55 @@ d = {'subject': list_df_subject,
      'race': list_df_race}
 df = pd.DataFrame(data=d)
 
+df.to_csv('./data/subject_info/clean/ADNI_info.csv', index=False)
+
+# Verify the values according to the new csv we received
+df = pd.read_csv('./data/subject_info/clean/ADNI_info.csv')
+df_new = pd.read_csv('./data/subject_info/raw/ADNI/ADNI_Prepped_for_Harmonization_Fall2023_Release_Covariates_Only.csv')
+
+dict_dx2previouscoding = {
+    1: 'normal',
+    2: 'MCI',	
+    3: "AD",
+    'NA': 'Not available or Other Dementia (not AD)'
+}
+
+for _,row in df_new.iterrows():
+    subject = "sub-{0:04d}".format(row['Phenotype_ID'])
+    session = f"ses-{row['Session']}"
+    age = row['PHC_Age_Imaging']
+    dx = row['PHC_Diagnosis']
+    try:
+        dx = dict_dx2previouscoding[dx]
+    except:
+        dx = None
+        
+    # Check if the session exists in BIDS
+    if len(df.loc[(df['subject']==subject)&(df['session']==session), ].index) == 0:
+        print("Extra sessions that do not exist in harmonization/BIDS")
+        continue
+    else:
+        # verify age
+        if df.loc[(df['subject']==subject)&(df['session']==session), 'age'].values.shape[0] == 0:
+            print('Use the age in the new csv to fill the empty cell: ')
+            print(subject, session, age)
+            df.loc[(df['subject']==subject)&(df['session']==session), 'age'] = age
+        else:
+            if abs(df.loc[(df['subject']==subject)&(df['session']==session), 'age'].values[0] - age) > 0.2:
+                print('Found large discrepancy in age:')
+                print(subject, session, age)
+                df.loc[(df['subject']==subject)&(df['session']==session), 'age'] = age
+            
+        # verify diagnosis
+        if df.loc[(df['subject']==subject)&(df['session']==session), 'diagnosis'].values.shape[0] == 0:
+            print('Use the diagnosis in the new csv (could also be None) to fill the empty cell: ')
+            print(subject, session, dx)
+            df.loc[(df['subject']==subject)&(df['session']==session), 'diagnosis'] = dx
+        else:
+            dx_previous = df.loc[(df['subject']==subject)&(df['session']==session), 'diagnosis'].values[0]
+            if (dx_previous != dx) and (dx != None):
+                print('Found different diagnosis for {} {}, previous: {}, updated: {}'.format(subject,session, dx_previous, dx))
+                df.loc[(df['subject']==subject)&(df['session']==session), 'diagnosis'] = dx
+
+# Save the updated csv
 df.to_csv('./data/subject_info/clean/ADNI_info.csv', index=False)
