e6744d6eae1a9f495a2e7086768c59191d70b9e3
chenyugoal <chenyu_gao@outlook.com>
2023-11-13 11:47:58 -0600

    verify ROSMAPMARS demog info according to the new csv of covariates

--- data/subject_info/raw/ROSMAPMARS/notes.md
+++ data/subject_info/raw/ROSMAPMARS/notes.md
@@ -1,3 +1,7 @@
+++++++++ updated on Nov 9, 2023
+Received new csv of ROSMAPMARS covariates cleaned by our collaborator: ./ROSMAPMARS_Prepped_for_Harmonization_Fall2023_Release_Covariates_Only.csv
+
+++++++++ updated on Sep 4, 2023
 `spreadsheet`:  ./data/subject_info/raw/ROSMAPMARS/ROSMAP_Covariates_08312023.csv
 `dictionary`: 
 - dx: 1/2/3 -- Cognitively unimpaired, mild cognitive impairment, Alzheimer's disease
--- data/subject_info/scripts/ROSMAPMARS_collect_info.py
+++ data/subject_info/scripts/ROSMAPMARS_collect_info.py
@@ -3,7 +3,6 @@
 # Author: Chenyu Gao
 # Date: Sept 4, 2023
 
-import json
 import pandas as pd
 from pathlib import Path
 
@@ -82,4 +81,56 @@ d = {'subject': list_df_subject,
 df = pd.DataFrame(data=d)
 df.sort_values(by=['subject', 'age'], inplace=True)
 
+df.to_csv('./data/subject_info/clean/ROSMAPMARS_info.csv', index=False)
+
+# updated on Nov 13, 2023:
+# verify the values according to the new csv we received
+df = pd.read_csv('./data/subject_info/clean/ROSMAPMARS_info.csv')
+df_new = pd.read_csv('./data/subject_info/raw/ROSMAPMARS/ROSMAPMARS_Prepped_for_Harmonization_Fall2023_Release_Covariates_Only.csv')
+
+dict_dx2previouscoding = {
+    1: 'normal',
+    2: 'MCI',	
+    3: "AD",
+    'NA': 'Not available or Other Dementia (not AD)'
+}
+
+for _,row in df_new.iterrows():
+    subject = "sub-{}".format(row['Phenotype_ID'])
+    session = f"ses-{row['Session']}"
+    age = row['PHC_Age_Imaging']
+    dx = row['PHC_Diagnosis']
+    try:
+        dx = dict_dx2previouscoding[dx]
+    except:
+        dx = None
+        
+    # Check if the session exists in BIDS
+    if len(df.loc[(df['subject']==subject)&(df['session']==session), ].index) == 0:
+        print("Extra sessions that do not exist in harmonization/BIDS. Will probably be useful in the future. {} {} {} {}".format(subject, session, age, dx))
+        continue
+    else:
+        # verify age
+        if df.loc[(df['subject']==subject)&(df['session']==session), 'age'].values.shape[0] == 0:
+            print('Use the age in the new csv to fill the empty cell: {} {} {}'.format(subject, session, age))
+            df.loc[(df['subject']==subject)&(df['session']==session), 'age'] = age
+        else:
+            if abs(df.loc[(df['subject']==subject)&(df['session']==session), 'age'].values[0] - age) > 0.2:
+                print('Found large discrepancy in age. {} {} Previous: {}, Updated: {}'.format(subject, 
+                                                                                               session, 
+                                                                                               df.loc[(df['subject']==subject)&(df['session']==session), 'age'].values[0], 
+                                                                                               age))
+                df.loc[(df['subject']==subject)&(df['session']==session), 'age'] = age
+            
+        # verify diagnosis
+        if df.loc[(df['subject']==subject)&(df['session']==session), 'diagnosis'].values.shape[0] == 0:
+            print('Use the diagnosis in the new csv (could also be None) to fill the empty cell: {} {} {}'.format(subject, session, dx))
+            df.loc[(df['subject']==subject)&(df['session']==session), 'diagnosis'] = dx
+        else:
+            dx_previous = df.loc[(df['subject']==subject)&(df['session']==session), 'diagnosis'].values[0]
+            if (dx_previous != dx) and (dx != None):
+                print('Found different diagnosis for {} {}, previous: {}, updated: {}'.format(subject, session, dx_previous, dx))
+                df.loc[(df['subject']==subject)&(df['session']==session), 'diagnosis'] = dx
+
+# Save the updated csv
 df.to_csv('./data/subject_info/clean/ROSMAPMARS_info.csv', index=False)
