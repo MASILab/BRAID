50d9f013b62c73e2077accd751fb38ccfab5a03d
chenyugoal <chenyu_gao@outlook.com>
2023-11-16 18:06:09 -0600

    Include PreQual path when merging datasets. Add HCPA to the databank.

--- data/dataset_splitting/scripts/combine_datasets_collect_imaging.py
+++ data/dataset_splitting/scripts/combine_datasets_collect_imaging.py
@@ -1,5 +1,9 @@
-# Should run this script on hickory to access GDPR (UKBB).
+# Should run this script on hickory to access GDPR (for UKBB).
 # Combine all datasets in the data bank and collect path information along with demographic information.
+# The following path information will be collected:
+#   - WMAtlas registration: so we can reuse the transformation matrix and the brain mask
+#   - PreQual: in case we need to do the registration, we can start from PreQualed data
+#   - T1w image: for future comparison between WM and GM age prediction
 # 
 # Author: Chenyu Gao
 # Date: Nov 14, 2023
@@ -22,47 +26,40 @@ list_df_diagnosis_original = [[],[]]
 list_df_diagnosis_detail = [[],[]]
 list_df_control_label = [[],[]]  # 1 for control, 0 for patient
 list_df_t1w_path = []  # Path to T1w image (not necessarily the one used for PreQual). We will probably need it for comparison between WM and GM age prediction.
-list_df_dti_registration_folder = []
+list_df_prequal_folder = []  # PreQual
+list_df_wmatlas_folder = []  # WMAtlas
 
-def save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race_original, age, dx_simple, dx, dx_detail, control_label, path, modality='DTI'):
+def save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race_original, age, dx_simple, dx, dx_detail, control_label, path, path_opt, modality='DTI'):
     global list_df_dataset, list_df_subject, list_df_session, list_df_scan, list_df_sex
     global list_df_race_simple, list_df_race_original, list_df_age
     global list_df_diagnosis_simple, list_df_diagnosis_original, list_df_diagnosis_detail
-    global list_df_control_label, list_df_t1w_path, list_df_dti_registration_folder
+    global list_df_control_label, list_df_t1w_path, list_df_prequal_folder, list_df_wmatlas_folder
     
     if modality == 'DTI':
-        list_df_dataset[0].append(dataset_name)
-        list_df_subject[0].append(subject)
-        list_df_session[0].append(session)
-        list_df_scan[0].append(scan)
-        list_df_sex[0].append(sex)
-        list_df_race_simple[0].append(race_simple)
-        list_df_race_original[0].append(race_original)
-        list_df_age[0].append(age)
-        list_df_diagnosis_simple[0].append(dx_simple)
-        list_df_diagnosis_original[0].append(dx)
-        list_df_diagnosis_detail[0].append(dx_detail)
-        list_df_control_label[0].append(control_label)
-        list_df_dti_registration_folder.append(path)
-    
+        idx_list = 0
+        list_df_prequal_folder.append(path)
+        list_df_wmatlas_folder.append(path_opt)
     elif modality == 'T1w':
-        list_df_dataset[1].append(dataset_name)
-        list_df_subject[1].append(subject)
-        list_df_session[1].append(session)
-        list_df_scan[1].append(scan)
-        list_df_sex[1].append(sex)
-        list_df_race_simple[1].append(race_simple)
-        list_df_race_original[1].append(race_original)
-        list_df_age[1].append(age)
-        list_df_diagnosis_simple[1].append(dx_simple)
-        list_df_diagnosis_original[1].append(dx)
-        list_df_diagnosis_detail[1].append(dx_detail)
-        list_df_control_label[1].append(control_label)
+        idx_list = 1
         list_df_t1w_path.append(path)
-    
+        if path_opt is not None:
+            raise ValueError("T1w image should not have a path_opt.")
     else:
         raise ValueError("Invalid modality. Supported modalities are 'DTI' and 'T1w'.")
-    
+
+    list_df_dataset[idx_list].append(dataset_name)
+    list_df_subject[idx_list].append(subject)
+    list_df_session[idx_list].append(session)
+    list_df_scan[idx_list].append(scan)
+    list_df_sex[idx_list].append(sex)
+    list_df_race_simple[idx_list].append(race_simple)
+    list_df_race_original[idx_list].append(race_original)
+    list_df_age[idx_list].append(age)
+    list_df_diagnosis_simple[idx_list].append(dx_simple)
+    list_df_diagnosis_original[idx_list].append(dx)
+    list_df_diagnosis_detail[idx_list].append(dx_detail)
+    list_df_control_label[idx_list].append(control_label)
+        
 # Dictionary to remap diagnosis and race, lookup dataset location
 dict_dx2standard = {
     'normal': 'normal', 
@@ -107,6 +104,7 @@ dict_bids_location = {
     'VMAP': '/nfs2/harmonization/BIDS/VMAP',
     'ADNI': '/nfs2/harmonization/BIDS/ADNI_DTI',
     'ICBM': '/nfs2/harmonization/BIDS/ICBM',
+    'HCPA': '/nfs2/harmonization/BIDS/HCPA',
 }
 
 # Folder of the cleaned csv
@@ -136,7 +134,8 @@ for csv in list_clean_csv:
         sex = row['sex']
         
         # dataset-specific information
-        if dataset_name in ['OASIS3', 'WRAP', 'BIOCARD', 'OASIS4', 'BLSA', 'NACC', 'ROSMAPMARS', 'VMAP', 'ADNI', 'ICBM']:
+        if dataset_name in ['OASIS3', 'WRAP', 'BIOCARD', 'OASIS4', 'BLSA', 'NACC', 'ROSMAPMARS', 'VMAP', 'ADNI', 'ICBM', 'HCPA']:
+            
             session = row['session']
             age = row['age']
             dx = row['diagnosis']
@@ -152,22 +151,28 @@ for csv in list_clean_csv:
                 control_label = 0
             
             # DTI
-            derivatives_folder = (dataset_path / 'derivatives' / subject / session) if dataset_name != 'ICBM' else (dataset_path / 'derivatives' / subject)
+            derivatives_folder = (dataset_path / 'derivatives' / subject) if dataset_name in ['ICBM', 'HCPA'] else (dataset_path / 'derivatives' / subject / session)
             if derivatives_folder.is_dir():
-                list_wmatlas = [fd for fd in derivatives_folder.iterdir() if ('WMAtlas' in fd.name) and ('EVE3' not in fd.name) and ('DTIdouble' not in fd.name)]
+                list_prequal = [fd for fd in derivatives_folder.iterdir() if ('PreQual' in fd.name) and ('DTIdouble' not in fd.name) and (fd/'PREPROCESSED'/'dwmri.nii.gz').is_file()]
             else:
-                list_wmatlas = []
-            list_wmatlas = sorted(list_wmatlas)
-            for i, dti_registration_folder in enumerate(list_wmatlas):
-                # check if it is empty
-                fa = dti_registration_folder / 'dwmri%fa.nii.gz'
-                md = dti_registration_folder / 'dwmri%md.nii.gz'
+                list_prequal = []
+            list_prequal = sorted(list_prequal)
+            
+            for i, prequal_folder in enumerate(list_prequal):
+                scan = i + 1
+                # corresponding WMAtlas folder (if any)
+                wmatlas_folder = prequal_folder.parent / prequal_folder.name.replace('PreQual', 'WMAtlas')
+                fa = wmatlas_folder / 'dwmri%fa.nii.gz'
+                md = wmatlas_folder / 'dwmri%md.nii.gz'
                 if fa.is_file() and md.is_file():
-                    scan = i+1
-                    save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, dti_registration_folder, modality='DTI')
+                    save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, prequal_folder, wmatlas_folder, modality='DTI')
+                else:
+                    if dataset_name != 'HCPA': 
+                        print(wmatlas_folder, ' does not exist.')
+                    save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, prequal_folder, None, modality='DTI')
             
             # T1w
-            anat_folder = (dataset_path / subject / session / 'anat') if dataset_name != 'ICBM' else (dataset_path / subject / 'anat')
+            anat_folder = (dataset_path / subject / 'anat') if dataset_name in ['ICBM', 'HCPA'] else (dataset_path / subject / session / 'anat')
             if anat_folder.is_dir():
                 list_t1w = [fn for fn in anat_folder.iterdir() if '_T1w.nii' in fn.name]
             else:
@@ -175,7 +180,7 @@ for csv in list_clean_csv:
             
             for i, t1w in enumerate(list_t1w):
                 scan = i+1
-                save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, t1w, modality='T1w')
+                save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, t1w, None, modality='T1w')
 
         elif dataset_name == 'UKBB':
             session = None
@@ -188,17 +193,22 @@ for csv in list_clean_csv:
             # DTI
             derivatives_folder = dataset_path / 'derivatives' / subject
             if derivatives_folder.is_dir():
-                list_wmatlas = [fd for fd in derivatives_folder.iterdir() if ('WMAtlas' in fd.name) and ('EVE3' not in fd.name) and ('DTIdouble' not in fd.name)]
+                list_prequal = [fd for fd in derivatives_folder.iterdir() if ('PreQual' in fd.name) and ('DTIdouble' not in fd.name) and (fd/'PREPROCESSED'/'dwmri.nii.gz').is_file()]
             else:
-                list_wmatlas = []
-            list_wmatlas = sorted(list_wmatlas)
-            for i, dti_registration_folder in enumerate(list_wmatlas):
-                # check if it is empty
-                fa = dti_registration_folder / 'dwmri%fa.nii.gz'
-                md = dti_registration_folder / 'dwmri%md.nii.gz'
+                list_prequal = []
+            list_prequal = sorted(list_prequal)
+
+            for i, prequal_folder in enumerate(list_prequal):
+                scan = i + 1
+                # corresponding WMAtlas folder (if any)
+                wmatlas_folder = prequal_folder.parent / prequal_folder.name.replace('PreQual', 'WMAtlas')
+                fa = wmatlas_folder / 'dwmri%fa.nii.gz'
+                md = wmatlas_folder / 'dwmri%md.nii.gz'
                 if fa.is_file() and md.is_file():
-                    scan = i+1
-                    save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, dti_registration_folder, modality='DTI')
+                    save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, prequal_folder, wmatlas_folder, modality='DTI')
+                else:
+                    print(wmatlas_folder, ' does not exist.')
+                    save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, prequal_folder, None, modality='DTI')
             
             # T1w
             anat_folder = dataset_path / subject / 'anat'
@@ -209,8 +219,8 @@ for csv in list_clean_csv:
             
             for i, t1w in enumerate(list_t1w):
                 scan = i+1
-                save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, t1w, modality='T1w')
-            
+                save_to_lists(dataset_name, subject, session, scan, sex, race_simple, race, age, dx_simple, dx, dx_detail, control_label, t1w, None, modality='T1w')
+
         else:
             print(dataset_name, ' is not supported yet.')
 
@@ -228,7 +238,8 @@ df_dti = pd.DataFrame({
     'diagnosis_original': list_df_diagnosis_original[0],
     'diagnosis_detail': list_df_diagnosis_detail[0],
     'control_label': list_df_control_label[0],
-    'dti_registration_folder': list_df_dti_registration_folder,
+    'prequal_folder': list_df_prequal_folder,
+    'wmatlas_folder': list_df_wmatlas_folder,
 })
 df_t1w = pd.DataFrame({
     'dataset': list_df_dataset[1],
@@ -243,7 +254,7 @@ df_t1w = pd.DataFrame({
     'diagnosis_original': list_df_diagnosis_original[1],
     'diagnosis_detail': list_df_diagnosis_detail[1],
     'control_label': list_df_control_label[1],
-    't1w_nifti': list_df_t1w_path,
+    't1w': list_df_t1w_path,
 })
 
 # Save to csv. The outputs must be on GDPR since it contains UKBB rows.
