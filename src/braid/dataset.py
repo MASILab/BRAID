1866f149d25519d24090292be18c73b2f5aaddde
chenyugoal <chenyu_gao@outlook.com>
2023-12-19 01:37:50 -0600

    dataloader version_2.0 (generate a scan list first, then loop through the list in the dataloader)

--- src/braid/dataset.py
+++ src/braid/dataset.py
@@ -1,5 +1,10 @@
 import torch
-from torch.utils.data import Dataset
+import random
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path, PosixPath
+from torch.utils.data import Dataset, DataLoader
 from monai.transforms import (
     Compose, 
     LoadImaged, 
@@ -10,10 +15,7 @@ from monai.transforms import (
     ToTensord,
     ConcatItemsd,
     )
-import pandas as pd
-import numpy as np
-import random
-from pathlib import Path, PosixPath
+
 
 def vectorize_sex_race(
     sex: str,
@@ -58,8 +60,8 @@ def vectorize_sex_race(
 
     return torch.cat((vec_sex, vec_race), dim=0)
 
-
-class BRAID_Dataset(Dataset):
+# This version is correct only when num_workers = 0
+class BRAID_Dataset_Single_Worker(Dataset):
     def __init__(
         self, 
         dataset_root: str | PosixPath,
@@ -141,3 +143,135 @@ class BRAID_Dataset(Dataset):
         age = torch.tensor(row['age'], dtype=torch.float32)
 
         return images, label_feature, age, row['scan_id']
+
+
+def get_the_sequence_of_scans(
+    csv_file: str | PosixPath,
+    subjects: np.ndarray | list[str] | str = 'all',
+    age_min: int = 45,
+    age_max: int = 90,
+    mode: str = 'train',
+    epochs: int = 1,
+    decay_factor: float = 0.01,
+) -> list[str] | list[list[str]]:
+    
+    # load and check the dataframe
+    df = pd.read_csv(csv_file)
+    columns_to_check = ['dataset', 'subject', 'session', 'scan', 'sex', 'race_simple', 'age', 'control_label', 'dataset_subject']
+    for col in columns_to_check:
+        if col not in df.columns: raise ValueError(f"Column '{col}' not found in csv file")
+    
+    # filter the dataframe by age and subjects
+    age_mask = (df['age'] >= age_min) & (df['age'] < age_max)
+    if type(subjects) == str:
+        if subjects == 'all': 
+            df = df.loc[age_mask, ]
+        else:
+            raise ValueError("subjects must be either 'all' or a list/array of subjects")
+    else:
+        if not '_' in subjects[0]: raise ValueError("subject format must be '{dataset}_{subject_id}'")
+        df = df.loc[age_mask & df['dataset_subject'].isin(subjects), ]
+    
+    df['scan_id'] = df['dataset_subject'] + '_' + df['session'] + '_' + 'scan-' + df['scan'].astype(str)
+    
+    if mode == 'train':
+        print('Generating the sequence of scans (with uniformly distributed ages) for training...')
+        list_scans = []
+        df['sample_weight'] = 1 / df.groupby('dataset_subject')['dataset_subject'].transform('count')
+        for epoch in tqdm(range(epochs)):
+            list_scans_epoch = []
+            for _ in range(df.shape[0]):
+                while True:
+                    age_start = random.choice(range(age_min, age_max))
+                    age_end = age_start + 1
+                    age_range_mask = (df['age'] >= age_start) & (df['age'] < age_end)
+                    samples = df.loc[age_range_mask, ]
+                    if samples.shape[0] >= 1:
+                        break
+                row = samples.sample(n=1, weights='sample_weight').iloc[0]
+                
+                # probability decay
+                df.loc[df['scan_id']==row['scan_id'], 'sample_weight'] *= decay_factor
+                df.loc[age_range_mask, 'sample_weight'] = df.loc[age_range_mask, 'sample_weight'] / df.loc[age_range_mask, 'sample_weight'].sum()
+
+                list_scans_epoch.append(row['scan_id'])
+            list_scans.append(list_scans_epoch)
+    else:
+        print('Generating the sequence of scans for testing...')
+        list_scans = df['scan_id'].tolist()
+    
+    return list_scans
+
+
+def flatten_the_list(lst):
+    flattened_list = []
+    for item in lst:
+        if isinstance(item, list):
+            flattened_list.extend(flatten_the_list(item))
+        else:
+            flattened_list.append(item)
+    return flattened_list
+
+
+class BRAID_Dataset(Dataset):
+    def __init__(
+        self,
+        dataset_root: str | PosixPath,
+        csv_file: str | PosixPath,
+        list_scans: list[str] | list[list[str]],
+    ) -> None:
+        
+        self.dataset_root = Path(dataset_root)
+        self.df = pd.read_csv(csv_file)
+        self.df['scan_id'] = self.df['dataset_subject'] + '_' + self.df['session'] + '_' + 'scan-' + self.df['scan'].astype(str)
+        self.list_scans = flatten_the_list(list_scans)
+    
+    def __len__(self):
+        return len(self.list_scans)
+    
+    def __getitem__(self, idx):
+        row = self.df.loc[self.df['scan_id']==self.list_scans[idx], ].iloc[0]
+
+        fa = self.dataset_root / row['dataset'] / row['subject'] / row['session'] / f"scan-{row['scan']}" / 'fa_skullstrip_MNI152.nii.gz'
+        md = self.dataset_root / row['dataset'] / row['subject'] / row['session'] / f"scan-{row['scan']}" / 'md_skullstrip_MNI152.nii.gz'
+        data_dict = {'fa': fa, 'md': md}
+        transform = Compose([
+            LoadImaged(keys=['fa', 'md'], image_only=False),
+            EnsureChannelFirstd(keys=['fa', 'md']),
+            Orientationd(keys=['fa', 'md'], axcodes="RAS"),
+            CenterSpatialCropd(keys=['fa', 'md'], roi_size=(192, 228, 192)),
+            Spacingd(keys=['fa', 'md'], pixdim=(1.5, 1.5, 1.5), mode=('bilinear', 'bilinear')),  # expected: 128 x 152 x 128, 1.5mm^3
+            ToTensord(keys=['fa', 'md']),
+            ConcatItemsd(keys=['fa', 'md'], name='images')  
+        ])
+        data_dict = transform(data_dict)
+        images = data_dict['images']
+        
+        label_feature = vectorize_sex_race(row['sex'], row['race_simple'])
+        age = torch.tensor(row['age'], dtype=torch.float32)
+
+        return images, label_feature, age, row['scan_id']
+
+
+def get_BRAID_dataloader(
+    dataset_root: str | PosixPath,
+    csv_file: str | PosixPath,
+    list_scans: list[str] | list[list[str]],    
+    batch_size: int = 8,
+    shuffle: bool = False,
+    num_workers: int = 8,
+    pin_memory: bool = True,
+    prefetch_factor: int = 2,
+    ) -> DataLoader:
+
+    dataset = BRAID_Dataset(dataset_root, csv_file, list_scans)
+    dataloader = DataLoader(
+        dataset, 
+        batch_size = batch_size, 
+        shuffle = shuffle, 
+        num_workers = num_workers,
+        pin_memory = pin_memory,
+        prefetch_factor = prefetch_factor,
+        )
+    
+    return dataloader
--- train.py
+++ train.py
@@ -7,22 +7,23 @@ Date: Dec 11, 2023
 
 import gc
 import yaml
+import math
 import argparse
 import torch
 import subprocess
 import numpy as np
 from pathlib import Path
 from tqdm import tqdm
-from torch.utils.data import DataLoader
-from braid.dataset import BRAID_Dataset
+from braid.dataset import get_the_sequence_of_scans, get_BRAID_dataloader
 from braid.models import get_the_resnet_model
 from braid.utls import generate_png_during_training
 from torch.utils.tensorboard import SummaryWriter
 
+device = torch.device('cuda')
+PNG_GENERATED = False
+
 if __name__ == "__main__":  
-    device = torch.device('cuda')
-    PNG_GENERATED = False
-    
+
     # User input: yaml file of the training configuration
     parser = argparse.ArgumentParser()
     parser.add_argument("--yaml", type=str, help="Path to yaml file, example: ./models/2023-12-11_ResNet50/train_config.yaml")
@@ -40,37 +41,36 @@ if __name__ == "__main__":
             print(f"Fold-{fold_idx} does not have valid subjects splitting.")
             continue
         
-        # Dataloader
-        dataset_train = BRAID_Dataset(
-            dataset_root = config['dataset']['root'],
+        # dataloader
+        list_scans_train = get_the_sequence_of_scans(
             csv_file = config['dataset']['csv_file'],
             subjects = subjects_train,
             age_min = config['dataset']['age_min'],
             age_max = config['dataset']['age_max'],
             mode = 'train',
+            epochs = config['optimization']['epochs'],
         )
-        dataset_val = BRAID_Dataset(
-            dataset_root = config['dataset']['root'],
+        list_scans_val = get_the_sequence_of_scans(
             csv_file = config['dataset']['csv_file'],
             subjects = subjects_val,
             age_min = config['dataset']['age_min'],
             age_max = config['dataset']['age_max'],
             mode = 'test',
+            epochs = None,
         )
-        dataloader_train = DataLoader(
-            dataset_train, 
-            batch_size = config['optimization']['batch_size'], 
-            shuffle = True, 
-            num_workers = config['optimization']['num_workers'],
-            pin_memory = True
-            )
-        dataloader_val = DataLoader(
-            dataset_val, 
-            batch_size = config['optimization']['batch_size'], 
-            shuffle = False, 
-            num_workers = config['optimization']['num_workers'],
-            pin_memory = True
-            )
+        dataloader_train = get_BRAID_dataloader(
+            dataset_root = config['dataset']['root'],
+            csv_file = config['dataset']['csv_file'],
+            list_scans = list_scans_train,    
+            batch_size = config['optimization']['batch_size'],
+            ) 
+        dataloader_val = get_BRAID_dataloader(
+            dataset_root = config['dataset']['root'],
+            csv_file = config['dataset']['csv_file'],
+            list_scans = list_scans_val,
+            batch_size = config['optimization']['batch_size'],
+        )
+        num_batches_per_epoch = round(len(dataloader_train)/config['optimization']['epochs'])  # we flatten all "epochs" into one "epoch"
 
         # Model
         model = get_the_resnet_model(
@@ -93,8 +93,7 @@ if __name__ == "__main__":
             scheduler = torch.optim.lr_scheduler.OneCycleLR(
                 optimizer = optimizer,
                 max_lr = config['optimization']['lr_scheduler']['lr_max'],
-                epochs = config['optimization']['epochs'],
-                steps_per_epoch = round(len(dataset_train)/(config['optimization']['batch_size']*config['optimization']['batch_size_mimic_factor'])),
+                total_steps= round(len(dataloader_train) / config['optimization']['batch_size_mimic_factor']),
                 cycle_momentum = config['optimization']['lr_scheduler']['cycle_momentum'],
             )
         else:
@@ -115,71 +114,71 @@ if __name__ == "__main__":
         subprocess.run(['mkdir', '-p', weights_server_dir])
 
         print(f"Training fold-{fold_idx} of model {config['model']['name']}...")
-        for epoch in range(config['optimization']['epochs']):
-            model.train()
-            epoch_loss = 0.0
-            optimizer.zero_grad()
-            for i, (images, label_feature, age, _) in enumerate(tqdm(dataloader_train)):
-                images, label_feature, age = images.to(device, non_blocking=True), label_feature.to(device, non_blocking=True), age.to(device, non_blocking=True)
-                
-                if (i+1) % 400 == 0:
-                    gc.collect()
-                    torch.cuda.empty_cache()
-                    if (config['output']['png_sanity_check'] != '') and (PNG_GENERATED == False):
-                        generate_png_during_training(
-                            img_tensor = images[0,:,:,:,:].cpu(), 
-                            path_png = Path(config['output']['png_sanity_check']) / f"age-{age[0].cpu().numpy():.1f}.png"
-                        )
-                        PNG_GENERATED = True
-                            
-                with torch.autocast('cuda'):
-                    output = model(images, label_feature)
-                    loss = loss_fn(output, age.view(-1, 1))
-                    epoch_loss += loss.detach()
+        model.train()
+        epoch_loss = 0.0
+        optimizer.zero_grad()
+        for i, (images, label_feature, age, _) in enumerate(tqdm(dataloader_train)):
+            images, label_feature, age = images.to(device, non_blocking=True), label_feature.to(device, non_blocking=True), age.to(device, non_blocking=True)
 
-                loss /= config['optimization']['batch_size_mimic_factor']
-                loss.backward()
+            if (i+1) % 1000 == 0:
+                gc.collect()
+                torch.cuda.empty_cache()
+                if (config['output']['png_sanity_check'] != '') and (PNG_GENERATED == False):
+                    generate_png_during_training(
+                        img_tensor = images[0,:,:,:,:].cpu(), 
+                        path_png = Path(config['output']['png_sanity_check']) / f"age-{age[0].cpu().numpy():.1f}.png"
+                    )
+                    PNG_GENERATED = True
+                        
+            with torch.autocast('cuda'):
+                output = model(images, label_feature)
+                loss = loss_fn(output, age.view(-1, 1))
+                epoch_loss += loss.detach()
 
-                if (i+1) % config['optimization']['batch_size_mimic_factor'] == 0 or (i+1) == len(dataloader_train):
-                    optimizer.step()
-                    optimizer.zero_grad()
-                    scheduler.step()
-                    
-            epoch_loss = epoch_loss / len(dataloader_train)
-            writer.add_scalar('Train/Loss', epoch_loss, epoch)
+            loss /= config['optimization']['batch_size_mimic_factor']
+            loss.backward()
 
-            # validation
-            model.eval()
-            val_loss = 0.0
-            with torch.no_grad():
-                for images, label_feature, age, _ in tqdm(dataloader_val):
-                    images, label_feature, age = images.to(device, non_blocking=True), label_feature.to(device, non_blocking=True), age.to(device, non_blocking=True)
-
-                    output = model(images, label_feature)
-                    loss_val = loss_fn(output, age.view(-1, 1))
-                    
-                    val_loss += loss_val.detach()
-
-            val_loss = val_loss / len(dataloader_val)
-            writer.add_scalar('Val/Loss', val_loss, epoch)
-
-            print("Epoch: {}\tCurrent LR: {}\tTrain Loss: {}\tValidation Loss: {}".format(epoch, scheduler.get_last_lr(), epoch_loss, val_loss))
+            if (i+1) % config['optimization']['batch_size_mimic_factor'] == 0 or (i+1) == len(dataloader_train):
+                optimizer.step()
+                optimizer.zero_grad()
+                scheduler.step()
             
-            # Save improved model to local
-            if val_loss < best_val_loss:
-                best_val_loss = val_loss
+            # Refresh epoch
+            if (i+1) % num_batches_per_epoch == 0 or (i+1) == len(dataloader_train):
+                epoch = math.ceil((i+1) / num_batches_per_epoch)
+                print(f"Epoch {epoch} finished.")
+                epoch_loss = epoch_loss / num_batches_per_epoch
+                writer.add_scalar('Train/Loss', epoch_loss, epoch)
+                epoch_loss = 0.0
+
+                model.eval()
+                val_loss = 0.0
+                with torch.no_grad():
+                    for images, label_feature, age, _ in tqdm(dataloader_val):
+                        images, label_feature, age = images.to(device, non_blocking=True), label_feature.to(device, non_blocking=True), age.to(device, non_blocking=True)
+
+                        output = model(images, label_feature)
+                        loss_val = loss_fn(output, age.view(-1, 1))
+                        
+                        val_loss += loss_val.detach()
+
+                val_loss = val_loss / len(dataloader_val)
+                writer.add_scalar('Val/Loss', val_loss, epoch)
+
+                print("Epoch: {}\tCurrent LR: {}\tTrain Loss: {}\tValidation Loss: {}".format(epoch, scheduler.get_last_lr(), epoch_loss, val_loss))
                 
-                path_pth_local = weights_local_dir / f"{config['model']['name']}_fold-{fold_idx}_epoch-{epoch}_valloss-{best_val_loss:.4f}.pth"
-                torch.save(model.state_dict(), path_pth_local)
-                print(f'Saved improved model to {path_pth_local} at epoch {epoch} with validation loss {best_val_loss}\n')
-        
+                # Save improved model to local
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    
+                    path_pth_local = weights_local_dir / f"{config['model']['name']}_fold-{fold_idx}_epoch-{epoch}_valloss-{best_val_loss:.4f}.pth"
+                    torch.save(model.state_dict(), path_pth_local)
+                    print(f'Saved improved model to {path_pth_local} at epoch {epoch} with validation loss {best_val_loss}\n')
+                
+                model.train()
+                
         path_pth_server = weights_server_dir / f"{config['model']['name']}_fold-{fold_idx}_epoch-{epoch}_valloss-{best_val_loss:.4f}.pth"
         print(f"Copying best model of fold-{fold_idx} to {path_pth_server}\n")
         subprocess.run(['cp', path_pth_local, path_pth_server])
         
         writer.close()
-
-
-
-
-
