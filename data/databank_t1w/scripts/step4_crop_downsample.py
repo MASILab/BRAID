a71d9a560bba13db85b1a165575e9b141255fb11
chenyugoal <chenyu_gao@outlook.com>
2024-01-30 10:27:42 -0600

    optimize speed

--- data/databank_t1w/scripts/step4_crop_downsample.py
+++ data/databank_t1w/scripts/step4_crop_downsample.py
@@ -2,6 +2,7 @@
 """
 
 import os
+import torch
 from tqdm import tqdm
 from pathlib import Path, PosixPath
 from torch.utils.data import Dataset, DataLoader
@@ -14,18 +15,18 @@ from monai.transforms import (
     Spacing,
     SaveImage,
 )
-
+torch.set_flush_denormal(True)
 
 def list_t1w_mni152(root_dir, suffix='_T1w_MNI152_Warped.nii.gz'):
     
-    list_t1w_mni152 = []
+    list_t1w = []
     for root, dirs, files in os.walk(root_dir):
         for fn in files:
             if fn.endswith(suffix):
-                list_t1w_mni152.append(os.path.join(root, fn))
+                list_t1w.append(os.path.join(root, fn))
     
-    print(f'Found {len(list_t1w_mni152)} T1w images in MNI152 space for {root_dir}')            
-    return list_t1w_mni152
+    print(f'Found {len(list_t1w)} T1w images in MNI152 space for {root_dir}')            
+    return list_t1w
 
 
 class Preprocessing_Dataset(Dataset):
@@ -65,20 +66,23 @@ if __name__ == "__main__":
     list_datasets = [fd for fd in databank_t1w.iterdir() if fd.is_dir()]
     
     for dataset_dir in list_datasets:
+        if dataset_dir.name in ['UKBB']:
+            print(f'Manually skip {dataset_dir.name}')
+            continue
 
-        list_t1w_mni152 = list_t1w_mni152(
+        list_t1w = list_t1w_mni152(
             root_dir=dataset_dir, 
             suffix='_T1w_MNI152_Warped.nii.gz'
             )
         
         dataset = Preprocessing_Dataset(
-            list_t1w=list_t1w_mni152,
+            list_t1w=list_t1w,
             data_root_dir=dataset_dir,
             )
 
         dataloader = DataLoader(
             dataset=dataset,
-            batch_size = 8,
+            batch_size = 4,
             shuffle = False,
             num_workers = 8,
             pin_memory = True,
