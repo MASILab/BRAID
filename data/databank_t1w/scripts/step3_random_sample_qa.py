66bdc0c0e06a2fa4cd112e37cc277e254d3da4c6
chenyugoal <chenyu_gao@outlook.com>
2024-02-05 11:06:39 -0600

    t1w qa screenshots

--- data/databank_t1w/scripts/step3_random_sample_qa.py
+++ data/databank_t1w/scripts/step3_random_sample_qa.py
@@ -1,44 +1,61 @@
+""" Randomly sample preprocessed T1w images and generate QA screenshots.
+Prepare a .csv file listing all selected samples.
+"""
+
 import os
 import random
+import subprocess
 import braid.utls
+import pandas as pd
 from tqdm import tqdm
+from pathlib import Path
 from multiprocessing import Pool
 
-def list_all_preprocessed_t1w(
-    databank_root='/nfs/masi/gaoc11/GDPR/masi/gaoc11/BRAID/data/databank_t1w',
-    suffix='_T1w_MNI152_Warped.nii.gz',
-    ):
-    
+def list_preprocessed_t1w(data_root, suffix):
     list_t1w = []
-    for root, dirs, files in os.walk(databank_root):
+    for root, dirs, files in os.walk(data_root):
         for fn in files:
             if fn.endswith(suffix):
                 list_t1w.append(os.path.join(root, fn))
                 
-    print(f"Found {len(list_t1w)} preprocessed T1w files.")
+    print(f"Found {len(list_t1w)} preprocessed T1w in {data_root}")
     return list_t1w
     
     
-def get_random_subset(list_t1w, n=200, seed=0):
-    
+def get_random_subset(list_t1w, n=500, seed=0):
+    n = len(list_t1w) if n > len(list_t1w) else n
+
     random.seed(seed)
     list_t1w_sample = random.sample(list_t1w, n)
-    
-    print(f"Randomly sampled {n} T1w files.")
     return list_t1w_sample
 
 
-def create_parallel_jobs(
-    list_t1w_sample,
-    qa_root = '/nfs/masi/gaoc11/projects/BRAID/data/databank_t1w/quality_assurance',
-):
-    
+def create_jobs(databank_root, suffix, n, seed, qa_root, qa_csv):
+    qa_root = Path(qa_root)
+    if not qa_root.is_dir():
+        subprocess.run(['mkdir', '-p', str(qa_root)])
+    path_qa_csv = qa_root / qa_csv
+
     list_job_tuples = []
-    for t1w in list_t1w_sample:
-        path_png = os.path.join(qa_root, t1w.split('/')[-1].replace('.nii.gz', '.png'))
-        list_job_tuples.append((t1w, path_png))
+    list_df_t1w = []
+
+    for dataset in Path(databank_root).iterdir():
+        if not dataset.is_dir():
+            continue
+        candidates = list_preprocessed_t1w(data_root=dataset, suffix=suffix)
+        list_t1w_sample = get_random_subset(candidates, n=n, seed=seed)
+        list_df_t1w.extend(list_t1w_sample)
+        
+        for t1w in list_t1w_sample:
+            path_png = qa_root / dataset.name / t1w.split('/')[-1].replace('.nii.gz', '.png')
+            list_job_tuples.append((t1w, path_png))
     
-    print(f"Created {len(list_job_tuples)} jobs.")
+    # save the selected samples to csv
+    d = {'t1w': list_df_t1w}
+    df = pd.DataFrame(data=d)
+    df['qa'] = None
+    df.to_csv(path_qa_csv, index=False)
+    
     return list_job_tuples
 
 
@@ -48,9 +65,15 @@ def generate_qa_screenshots(tuple):
 
 
 if __name__ == '__main__':
-    list_t1w = list_all_preprocessed_t1w()
-    list_t1w_sample = get_random_subset(list_t1w)
-    list_job_tuples = create_parallel_jobs(list_t1w_sample)
+    list_job_tuples = create_jobs(
+        databank_root = '/nfs/masi/gaoc11/GDPR/masi/gaoc11/BRAID/data/databank_t1w', 
+        suffix = '_T1w_brain_MNI152_Warped.nii.gz', 
+        n = 500, 
+        seed = 0, 
+        qa_root = '/nfs/masi/gaoc11/projects/BRAID/data/databank_t1w/quality_assurance/2024-02-05_brain_affine', 
+        qa_csv = 'qa.csv')
     
     with Pool(processes=8) as pool:
-        results = list(tqdm(pool.imap(generate_qa_screenshots, list_job_tuples, chunksize=1), total=len(list_job_tuples), desc='Generating QA screenshots'))
+        list(tqdm(pool.imap(generate_qa_screenshots, list_job_tuples, chunksize=1), 
+                  total=len(list_job_tuples), 
+                  desc='Generating QA screenshots'))
