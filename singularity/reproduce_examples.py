e501d60e5c5bcbefbee4e3783a2c18d3ebf811b5
chenyugoal <chenyu_gao@outlook.com>
2025-03-20 13:46:06 -0500

    verify the singularity and its reproducibility

--- singularity/reproduce_examples.py
+++ singularity/reproduce_examples.py
@@ -1,5 +1,6 @@
 import json
 import subprocess
+import pandas as pd
 from tqdm import tqdm
 from pathlib import Path
 
@@ -27,22 +28,22 @@ def create_job_tuples(example_data_root):
 def run_braid_in_two_ways(job_tuple):
     dwi, bval, bvec, t1, t1_seg, age, sex, race, dir = job_tuple
     
-    # # run braid with python source code
-    # outdir = Path(dir) / 'braid_output_python'
-    # outdir.mkdir(parents=True, exist_ok=True)
-    # log = outdir / 'log.txt'
-    # cmd = [
-    #     'braid_one_sample_inference', 
-    #     '-d', dwi, '-v', bval, '-c', bvec, 
-    #     '-t', t1, '-tm', t1_seg,
-    #     '-m', 'data/template/MNI_152.nii.gz',
-    #     '-a', str(age), '-s', str(sex), '-r', str(race),
-    #     '-w', '/home-local/gaoc11/braid_go_public/braid-v1.0', 
-    #     '-i', 
-    #     '-o', outdir
-    # ]
-    # with open(log, 'w') as f:
-    #     subprocess.run(cmd, stdout=f, stderr=f)
+    # run braid with python source code
+    outdir = Path(dir) / 'braid_output_python'
+    outdir.mkdir(parents=True, exist_ok=True)
+    log = outdir / 'log.txt'
+    cmd = [
+        'braid_one_sample_inference', 
+        '-d', dwi, '-v', bval, '-c', bvec, 
+        '-t', t1, '-tm', t1_seg,
+        '-m', 'data/template/MNI_152.nii.gz',
+        '-a', str(age), '-s', str(sex), '-r', str(race),
+        '-w', '/home-local/gaoc11/braid_go_public/braid-v1.0', 
+        '-i', 
+        '-o', outdir
+    ]
+    with open(log, 'w') as f:
+        subprocess.run(cmd, stdout=f, stderr=f)
     
     # run braid with singularity
     outdir = Path(dir) / 'braid_output_singularity'
@@ -57,8 +58,24 @@ def run_braid_in_two_ways(job_tuple):
     with open(log, 'w') as f:
         subprocess.run(cmd, stdout=f, stderr=f)
 
-
+def compare_outputs(example_data_root):
+    example_data_root = Path(example_data_root)
+    for i in [1,2,3]:
+        dir = example_data_root / f'example_{i}'
+        print(f"Source code vs. singularity run for {dir}")
+        csv_sc = dir / 'braid_output_python' / 'final' / 'braid_predictions.csv'
+        csv_sg = dir / 'braid_output_singularity' / 'final' / 'braid_predictions.csv'
+        df_sc = pd.read_csv(csv_sc)
+        df_sg = pd.read_csv(csv_sg)
+        for col in df_sc.columns:
+            if col in ['path_dwi', 'path_t1w']:
+                continue
+            print(f"{df_sc[col].values[0]:.3f}\t{df_sg[col].values[0]:.3f}\t{col}")
+        print("##############################################")
+        
+        
 if __name__ == "__main__":
     job_tuples = create_job_tuples('singularity/example_data')
     for job_tuple in tqdm(job_tuples, desc="BRAID source code / singularity run"):
         run_braid_in_two_ways(job_tuple)
+    compare_outputs('singularity/example_data')
