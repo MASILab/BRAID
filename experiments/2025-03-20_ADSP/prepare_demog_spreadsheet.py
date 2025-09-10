01d7dc9ca83369df42af3325511defee51299f97
chenyugoal <chenyu_gao@outlook.com>
2025-09-09 20:20:46 -0500

    refactor code

--- experiments/2025-03-20_ADSP/prepare_demog_spreadsheet.py
+++ experiments/2025-03-20_ADSP/prepare_demog_spreadsheet.py
@@ -1,7 +1,34 @@
 import pdb
 import json
+import argparse
 import pandas as pd
 from pathlib import Path
+
+lut_sex = {
+    'female': 0,
+    'male': 1,
+    0: 0, 
+    1: 1,
+    '0': 0,
+    '1': 1,
+}
+
+lut_race = {
+    'NHW': 1,
+    'White': 1, 
+    'Asian': 2, 
+    'NHB': 3,
+    'Black': 3,
+    'Black or African American': 3, 
+    'American Indian or Alaska Native': 4,
+    'American Indian or Alaskan Native': 4,
+    'More than one race': 0, 
+    'Native Hawaiian or Other Pacific Islander': 0,
+    'Hispanic or Latino': 0,
+    'Other': 0,
+    'Other, Unknown, or More than one race': 0,
+    'Unknown': 0,
+}
 
 
 def extract_dwi_tabular_adni_6_10_25(
@@ -48,93 +75,85 @@ def extract_dwi_tabular_adni_6_10_25(
     df.to_csv(output_csv, index=False)
 
 
-if __name__ == '__main__':
-    extract_dwi_tabular_adni_6_10_25()
-
-
-# # csv file name: (dataset name in BIDS, is cross-sectional)
-# manually_checked = {
-#     # 'ADNI_info.csv': ('ADNI_DTI', False),
-#     # 'ICBM_info.csv': ('ICBM', True),
-#     # 'BIOCARD_info.csv': ('BIOCARD', False),
-#     # 'ROSMAPMARS_info.csv': ('ROSMAPMARS', False),
-#     # 'NACC_info.csv': ('NACC', False),
-#     'BLSA_info.csv': ('BLSA', False),
-#     'OASIS3_info.csv': ('OASIS3', False),
-#     'OASIS4_info.csv': ('OASIS4', False),
-#     'WRAP_info.csv': ('WRAP', False),
-#     'HABSHD_info.csv': ('HABSHD', False),
-# }
-
-# lut_sex = {
-#     'female': 0,
-#     'male': 1,
-#     0: 0, 
-#     1: 1,
-# }
-
-# lut_race = {
-#     'White': 1, 
-#     'Asian': 2, 
-#     'Black or African American': 3, 
-#     'American Indian or Alaska Native': 4,
-#     'More than one race': 0, 
-#     'Native Hawaiian or Other Pacific Islander': 0,
-#     'Hispanic or Latino': 0,
-#     'Black': 3,
-#     'NHW': 1,
-#     'Other': 0,
-#     'NHB': 3,
-#     'Other, Unknown, or More than one race': 0,
-# }
-
-# path_bids = Path('/nfs2/harmonization/BIDS')
-# raw_dir = Path('experiments/2025-03-20_ADSP/raw')
-# converted_dir = Path('experiments/2025-03-20_ADSP/converted')
-
-# for fn_csv in manually_checked.keys():
-#     print(f"Converting {fn_csv}")
-#     df = pd.read_csv(raw_dir / fn_csv)
-#     print(f"\tNumber of rows (before): {len(df.index)}")
+def combine_csvs(
+        prev_csv: str = 'raw/ADNI_info.csv',
+        new_csv: str = 'raw/ADNI_6_10_25/bids.csv',
+        output_csv: str = 'raw/ADNI_info_combined.csv',
+    ):
+    """ Combine two csv files by concatenation and removing duplicates.
+    """
+    df_prev = pd.read_csv(prev_csv)
+    df_new = pd.read_csv(new_csv)
+    df_combined = pd.concat([df_prev, df_new], join='inner', ignore_index=True)
+    df_combined.drop_duplicates(keep='first', inplace=True)
     
-#     # Are all unique labels considered in the lookup tables
-#     for label in df['sex'].unique():
-#         if label not in lut_sex and not pd.isna(label):
-#             raise ValueError(f"sex label not considered: {label}")        
-#     for label in df['race'].unique():
-#         if label not in lut_race and not pd.isna(label):
-#             raise ValueError(f"race label not considered: {label}")
+    Path(output_csv).parent.mkdir(parents=True, exist_ok=True)
+    df_combined.to_csv(output_csv, index=False)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Prepare demographic spreadsheet for a BIDS dataset to run BRAID.')
+    parser.add_argument('-n', '--bids_name', type=str, required=True, help='Name of the dataset in BIDS.')
+    parser.add_argument('-c', '--cross_sectional', action='store_true', help='Whether the dataset is cross-sectional.')
+    parser.add_argument('-i', '--input_csv', type=str, required=True, help='Path to the csv to be converted.')
+    parser.add_argument('-o', '--outdir', type=str, required=False, default='./converted', help='Directory to save the converted csv.')
+    args = parser.parse_args()
+
+    path_dataset = Path(f'/nfs2/harmonization/BIDS/{args.bids_name}')
+    input_csv = Path(args.input_csv)
+    outdir = Path(args.outdir)
+    outdir.mkdir(parents=True, exist_ok=True)
+    output_csv = outdir / input_csv.name.replace('.csv', '_converted.csv')
+    
+    df = pd.read_csv(input_csv)
+    for col in ['subject','age','sex','race']:
+        assert col in df.columns, f"Column {col} not found in {input_csv}"
+    if not args.cross_sectional:
+        assert 'session' in df.columns, f"Column session not found in {input_csv}"
+
+    for label in df['sex'].unique():
+        if label not in lut_sex and not pd.isna(label):
+            raise NotImplementedError(f"Sex label not considered: {label}")
+    for label in df['race'].unique():
+        if label not in lut_race and not pd.isna(label):
+            raise NotImplementedError(f"Race label not considered: {label}")
+    
+    df['sex'] = df['sex'].apply(lambda x: lut_sex.get(x, None) if not pd.isna(x) else None)
+    df['race'] = df['race'].apply(lambda x: lut_race.get(x, None) if not pd.isna(x) else None)
+    
+    print("Scanning BIDS directory to include missing rows as placeholders...")
+    if args.cross_sectional:
+        bids_subjects = [{'subject': subject.name} for subject in path_dataset.glob('sub-*')]
+        if not bids_subjects:
+            raise FileNotFoundError(f"No subjects found in BIDS directory: {path_dataset}")
         
-#     df['sex'] = df['sex'].apply(lambda x: lut_sex.get(x, None) if not pd.isna(x) else None)
-#     df['race'] = df['race'].apply(lambda x: lut_race.get(x, None) if not pd.isna(x) else None)
-    
-#     # Make sure every session in BIDS has a row, even if the demographic information is missing
-#     path_dataset = path_bids / manually_checked[fn_csv][0]
-#     cross_sectional = manually_checked[fn_csv][1]
-#     assert 'subject' in df.columns, f"Column subject not found in {fn_csv}"
-#     if not cross_sectional:
-#         assert 'session' in df.columns, f"Column session not found in {fn_csv}"
-        
-#     for subject in path_dataset.glob('sub-*'):
-#         if not cross_sectional:
-#             for session in subject.glob('ses-*'):
-#                 if len(df.loc[(df['subject']==subject.name)&(df['session']==session.name), ].index) == 0:
-#                     new_row = {col: None for col in df.columns}
-#                     new_row['subject'] = subject.name
-#                     new_row['session'] = session.name
-#                     new_row = pd.DataFrame([new_row])
-#                     df = pd.concat([df, new_row], ignore_index=True)
-#         else:
-#             if len(df.loc[df['subject']==subject.name,].index) == 0:
-#                 new_row = {col: None for col in df.columns}
-#                 new_row['subject'] = subject.name
-#                 new_row = pd.DataFrame([new_row])
-#                 df = pd.concat([df, new_row], ignore_index=True)
-    
-#     # Remove duplicates
-#     subset = ['subject', 'session'] if not cross_sectional else ['subject']
-#     df = df.drop_duplicates(subset=subset, keep='first')
+        bids_df = pd.DataFrame(bids_subjects)
+        df = pd.merge(bids_df, df, on='subject', how='left')
+
+    else:
+        bids_sessions = [
+            {'subject': subject.name, 'session': session.name}
+            for subject in path_dataset.glob('sub-*')
+            for session in subject.glob('ses-*')
+        ]
+        if not bids_sessions:
+            raise FileNotFoundError(f"No sessions found in BIDS directory: {path_dataset}")
+
+        bids_df = pd.DataFrame(bids_sessions)
+        df = pd.merge(bids_df, df, on=['subject', 'session'], how='left')
+
+    id_cols = ['subject', 'session'] if not args.cross_sectional else ['subject']
+    df = df.drop_duplicates(subset=id_cols, keep='first')
                 
-#     new_csv = converted_dir / fn_csv.replace('.csv', '_converted.csv')
-#     df.to_csv(new_csv, index=False)
-#     print(f"\tNumber of rows (after): {len(df.index)}")
+    cols_to_keep = ['subject','session','age','sex','race']
+    df = df[cols_to_keep]
+    df.to_csv(output_csv, index=False)
+    print(f"Converted csv saved to {output_csv}")
+
+
+if __name__ == '__main__':
+    # extract_dwi_tabular_adni_6_10_25()
+    # combine_csvs()
+    
+    main()
+    
